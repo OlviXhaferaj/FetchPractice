9ef9b36a646f6bf4a68d3faa4acddd9415c8b34e
OlviXhaferaj <olvixhaferaj@gmail.com>
2022-11-22 19:37:13 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [state, setState]= useState([]);
+  
+  useEffect(()=>{
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) =>{
+      setState((response.results))
+      console.log(response.results )
+    })
+    .catch((err) => console.log(err))
+
+  }, [])
+  console.log(state)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      state.map((object,index)=>{
+        return(
+          <div key={index}>
+            <p>{index+1}. {object.name}</p>
+          </div>
+        )
+      })
+    }
     </div>
   );
 }
